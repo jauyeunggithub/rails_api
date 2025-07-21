0a5f1b78d991b8f70adf96d3372935315429d6a5
Ho Hang Au Yeung <mayeung@telus.net>
2025-07-21 16:54:53 -0700

    fix errors

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,9 @@
-require_relative "boot"
-require "rails/all"
-require 'active_support/logger'
-require 'active_support/core_ext/logger'
-
-
-Bundler.require(*Rails.groups)
+require 'rails/all'
 
 module App
   class Application < Rails::Application
-    config.load_defaults 7.1
-    config.autoload_lib(ignore: %w(assets tasks))
-
-    config.api_only = true
-
-    if Rails.env.test?
-      ENV['DISABLE_BOOTSNAP'] = '1'
-    end
+    # Use default logging configuration
+    config.logger = ActiveSupport::Logger.new(STDOUT)
+    config.log_level = :debug
   end
 end
