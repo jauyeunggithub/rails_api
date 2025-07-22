eea2268db651600513587d1e6afa9a857631582e
Ho Hang Au Yeung <mayeung@telus.net>
2025-07-21 18:28:34 -0700

    fix tests

--- test/mailers/post_mailer_test.rb
+++ test/mailers/post_mailer_test.rb
@@ -1,11 +1,27 @@
 require 'test_helper'
 
 class PostMailerTest < ActionMailer::TestCase
-  test "should send post created email" do
-    post = posts(:one)  # Assuming you have a fixture for post
-    email = PostMailer.with(post: post).post_created_email
+  def setup
+    @user = User.create!(email: "testuser@example.com")
+    @post = Post.create!(title: "Sample Post", content: "This is a test post", user: @user)
+  end
+
+  test "new_post_email sends email" do
+    email = PostMailer.new_post_email(@post)
+
     assert_emails 1 do
       email.deliver_now
     end
+
+    assert_equal ['no-reply@example.com'], email.from
+    assert_equal [@user.email], email.to
+    assert_equal "New Post: #{@post.title}", email.subject
+  end
+
+  test "new_post_email body includes post content" do
+    email = PostMailer.new_post_email(@post)
+    email.deliver_now
+
+    assert_includes email.body.to_s, @post.content
   end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,15 +1,16 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  test "should belong to post" do
-    post = posts(:one)
-    comment = post.comments.create(content: "Great post!")
-    assert_equal post, comment.commentable
+  test "should belong to a commentable object" do
+    post = Post.create!(title: "Test Post", content: "Test content")
+    comment = post.comments.create!(content: "Test comment")
+
+    assert_equal post, comment.commentable, "Comment's commentable should be the post"
+    assert_instance_of Post, comment.commentable, "Comment's commentable should be a Post instance"
   end
 
-  test "should belong to image" do
-    image = images(:one)  # Assuming you have a fixture for image
-    comment = image.comments.create(content: "Nice image!")
-    assert_equal image, comment.commentable
+  test "should not save comment without commentable" do
+    comment = Comment.new(content: "Test content without commentable")
+    assert_not comment.save, "Saved the comment without a commentable object"
   end
 end
